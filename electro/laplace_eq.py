21ad0b1500ea86ef0077e1581e6c1581b72000a8
Axel Alejandro Jimenez Reyes <73604021+axel-netizen@users.noreply.github.com>
2024-02-06 12:36:38 -0600

    Update laplace_eq.py

--- electro/laplace_eq.py
+++ electro/laplace_eq.py
@@ -1,6 +1,10 @@
-import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt 
+
+'''
+    This code presents the numerical solution to 2 bars or plates with 100 and -100 voltage. The 
+    the shape of metallic plates can be changed but for this situation, I'm using two bars
+'''
+
 #Initialize V0, the array of size NxN and dV 
 dV = 0
 n  = 100
@@ -14,7 +18,7 @@ V[20:-20,n-60]= 100
 #Metallic square 1
 #V[2:-2,4:-4]  = 10
 '''
-#Metallic square 2
+#Metallic square 2 with a voltage of 10 V and -10 V
 V[0,:]   = 10
 V[n-1,:] = 10
 V[:,0]   = 10
@@ -24,28 +28,16 @@ V[5:11,-6] = -10
 V[5:11,5]  = -10
 V[4, 5:11] = -10
 V[-5, 5:11] = -10
-'''
+'''     
 
-#error = 1
-'''
-while error > 0.001 :
-    for i in range(1,n-1):
-        for j in range(1,n-1):
-            Vav = V[i,j]
-            V[i,j] = (V[i-1,j] + V[i+1,j] + V[i,j-1] + V[i,j+1]) / 4
-            error = V[i,j] - Vav 
-            print(error)
-        
 
-'''
 while dV < 60:
     for i in range(1,n-1):
         for j in range(1,n-1):
             V[i,j] = (V[i-1,j] + V[i+1,j] + V[i,j-1] + V[i,j+1]) / 4
     dV += 1
-sns.heatmap(V, cmap="crest", annot=True)
-plt.imshow(V, cmap= 'Dark2' )
-#plt.contour(V,cmap = 'Blues')
+    
+plt.contour(V,cmap = 'Blues')
 plt.colorbar()
 
 plt.show()
